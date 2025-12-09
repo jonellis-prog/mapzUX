86caaf0e09435f5d0b48f6b7eccfec22322345a3
Jon F. Ellis <Jon.Ellis@omniFederal.com>
2025-12-09 14:04:09 -0600

    map almost renders - some javascript errors in eslint

--- mapux/src/App.js
+++ mapux/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Button, Row, card, Col, FormControl} from 'react-bootstrap';
+// import { MapContainer, TileLayer, Marker, Popup } from '../src/static/leaflet/leaflet-src2';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import ds from "./images/DeathStar.png";
 
 
+const MapComponent = () => {
+  const position = [51.505, -0.09];
+}
 function App() {
   return (
     <div className="bg-dark min-vh-100">
+        <br/>
         <Container className="bg-dark mt-12 tealtext">
                 <div className="row">
                     <div className="col-sm-2">
@@ -31,8 +38,22 @@ function App() {
                             <Button variant="primary" size="sm">Show!</Button>
                         </div>
                     </div>
-                </div>                                  
+                </div>  
+                <br />                                
         </Container>
+        <main>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="leaflet-container">
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" // Example tile provider URL
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+            </MapContainer>
+        </main>
 
     </div>
   );
