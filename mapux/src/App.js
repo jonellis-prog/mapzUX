cff9fb65a03c67249b367a5dabf3f55e42e1bc72
Jon F. Ellis <Jon.Ellis@omniFederal.com>
2025-12-10 13:55:31 -0600

    Convert to Bootstrap form with JS handler

--- mapux/src/App.js
+++ mapux/src/App.js
@@ -7,13 +7,17 @@
     import { defaults as defaultControls } from 'ol/control';
     import { fromLonLat } from 'ol/proj';
     import 'bootstrap/dist/css/bootstrap.min.css';  // from 'react-bootstrap';
-    import { Container, FormControl, Card, Button } from 'react-bootstrap';
+    import { Container, FormControl, Form, Card, Button } from 'react-bootstrap';
     
     import 'ol/ol.css'; // Import OpenLayers CSS
     import './App.css';
     import ds from "./images/DeathStar.png";
 
     const MapComponent = () => {
+      const [inputAddress, setInputAddress] = useState('');
+      const [inputX, setInputX] = useState('');
+      const [inputY, setInputY] = useState('');
+
       const mapRef = useRef();
       const [mouseCoordinates, setMouseCoordinates] = useState('');
 
@@ -31,8 +35,6 @@
         });
 
         const map = new Map({
-
-          
           controls: defaultControls().extend([mousePositionControl]),
           layers: [
             new TileLayer({
@@ -46,6 +48,13 @@
           target: mapRef.current,
         });
 
+        const GotoNewCoordinates = (event) =>{
+            console.warn('CHanging Coordinates');
+            map.setView(new View({
+            center: [inputX, inputY],
+            zoom: map.getView().getZoom() // Keep current zoom or set new
+        }))};
+
         // Update React state for external display if needed
         map.on('pointermove', (event) => {
           const coordinates = map.getEventCoordinate(event.originalEvent);
@@ -57,7 +66,7 @@
       }, []);
 
       return (
-        <div>
+        <div style={{ width: '100%'}}>
           <div style={{ width: '100%'}}>
                 <Container className="bg-dark mt-12 tealtext">
                         <div className="row">
@@ -65,29 +74,51 @@
                                 <img src={ds} height="64px"></img>
                                 <h3>DStar Maps</h3>
                             </div>
-                            <div className = "col-sm-6">
-                                
-                                        <div>
-                                            <label class="navteal">Optional: Find coordinates of an address</label>
-                                            <FormControl type="text" className="form-control-sm" />
-                                            <Button variant="primary" size="sm">Find Coordinates!</Button>
-                                        </div>  
-                            </div>                            
-                            <div class="col-sm-4">
+                            <div className = "col-sm-6">                             
                                 <div>
-                                    <label >Enter coordinates</label>
-                                    <FormControl type="text" className="form-control-sm" id="XCoord" title="X Coordinate" />
-                                    <FormControl type="text" className="form-control-sm" id="YCoord" title="Y coordinate" />
-                                    <Button variant="primary" size="sm">Show!</Button>
+                                    <label className="navteal">Optional: Find coordinates of an address</label>
+                                    <FormControl type="text" className="form-control-sm" />
+                                    <Button variant="primary" size="sm">Find Coordinates!</Button>
+                                </div>  
+                            </div>                            
+                            <div className="col-sm-4">
+                                <div>
+                                    <Form>  
+                                      <label >Enter coordinates</label>
+                                      <Form.Control 
+                                        type="text" 
+                                        className="form-control-sm" 
+                                        id="XCoord" 
+                                        title="X Coordinate" 
+                                        placeholder="Enter X Coordinate"
+                                        value={inputX}
+                                        onChange={(e) => setInputX(e.target.value)}/>
+
+                                        <Form.Control 
+                                        type="text" 
+                                        className="form-control-sm" 
+                                        id="YCoord" 
+                                        title="Y Coordinate" 
+                                        placeholder="Enter Y Coordinate"
+                                        value={inputY}
+                                        onChange={(e) => setInputY(e.target.value)}/>
+                                      <Button type="submit" variant="primary" size="sm">Show!</Button>
+                                     </Form>
                                 </div>
                             </div>
                         </div>  
                         <br />                                
                 </Container>
-            </div>  
+          </div>  
           <div ref={mapRef} style={{ width: '100%', height: '500px' }}></div>
           <div id="mouse-position" className="mouse-position-display">
             Mouse Position: {mouseCoordinates}
+          </div>
+          <div>
+            <hr></hr>
+            <span>
+              Form Inputs: {inputX}, {inputY}
+            </span>
           </div>
         </div>
       );
